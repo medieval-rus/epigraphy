fe3ebe0e905b8250d732209f52423967acb10387
Anton Dyshkant <vyshkant@gmail.com>
2021-07-20 03:30:04 +0300

    License text fix

--- src/Persistence/Repository/Epigraphy/InscriptionRepository.php
+++ src/Persistence/Repository/Epigraphy/InscriptionRepository.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
  * GNU General Public License as published by the Free Software Foundation, version 3.
  *
  * «Epigraphy of Medieval Rus» database is distributed
- * in the hope  that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
  * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
